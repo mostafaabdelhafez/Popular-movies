26e9ab5b9302a896405f9aa86a9770c97096f06b
Mostafa Abdelhafez <mostafa.aladar@jobzella.com>
2024-12-02 14:52:04 +0200

    Initial Commit

---
+++ PopularMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PopularMovies
+//
+//  Created by jobzella on 12/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
